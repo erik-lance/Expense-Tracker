745708d9ba77ab1e4486cd9c5fc1e16862552b53
erik-lance <eriklance@gmail.com>
2023-08-22 11:33:47 +0800

    Added view controller

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Expense_Tracker.Core;
+using System;
 
 namespace Expense_Tracker.MVVM.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : ObservableObject
     {
+        public AddDataViewModel AddDataVm { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            AddDataVm = new AddDataViewModel();
+            CurrentView = AddDataVm;
+        }
+
     }
 }
