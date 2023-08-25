57713689f5b13fa28dfec2af328e1d28332e1fab
erik-lance <eriklance@gmail.com>
2023-08-25 18:08:35 +0800

    Setup data access class for SQLite

---
+++ Data/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Transactions;
+
+namespace Expense_Tracker.Data
+{
+    internal class DataAccess
+    {
+        private readonly string _connectionString;
+
+        public DataAccess()
+        {
+            // Get Directory
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string dbFilePath = Path.Combine(baseDir, "Data", "expense_data.db");
+            _connectionString = $"Data Source={dbFilePath};Version=3;";
+        }
+
+        public void InsertTransaction(Transaction transaction)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            using (var command = new SQLiteCommand(connection))
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
