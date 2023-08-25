1d64eed2c0e7896b019213312cf89c2580390ac4
erik-lance <eriklance@gmail.com>
2023-08-25 18:28:54 +0800

    Added model for transaction

---
+++ MVVM/Model/TransactionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expense_Tracker.MVVM.Model
+{
+    internal class TransactionModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Category { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
