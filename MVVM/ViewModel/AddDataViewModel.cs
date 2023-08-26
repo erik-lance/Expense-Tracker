7a6424eb4244ff1cc05dbc0faf732f84f8a127ad
erik-lance <eriklance@gmail.com>
2023-08-26 13:16:26 +0800

    Added initial AddTransaction skeleton

--- MVVM/ViewModel/AddDataViewModel.cs
+++ MVVM/ViewModel/AddDataViewModel.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using Expense_Tracker.Core;
+using System;
 using System.Collections.ObjectModel;
 using System.Transactions;
+using System.Windows.Input;
 
 namespace Expense_Tracker.MVVM.ViewModel
 {
     internal class AddDataViewModel
     {
+        public ICommand AddTransactionCommand { get; }
         public ObservableCollection<Transaction> RecentTransactions { get; set; }
 
         public AddDataViewModel()
         {
+            AddTransactionCommand = new RelayCommand(AddTransaction);
+
             // Initialize RecentTransactions
             RecentTransactions = new ObservableCollection<Transaction>();
+        }
+
+        private void AddTransaction(object param)
+        {
+            if (param is Transaction newTransaction)
+            {
+                // Add new transaction to RecentTransactions
+                // RecentTransactions.Add(newTransaction);
+
+                // Add new transaction to database
+            }
+
         }
     }
 }
