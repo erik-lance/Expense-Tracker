334e5456e59d24bf19069676189c11ac1aaabc96
erik-lance <eriklance@gmail.com>
2023-08-26 13:51:01 +0800

    Prepared dependency injection service

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using Expense_Tracker.Data;
+using Expense_Tracker.MVVM.ViewModel;
+using Expense_Tracker.MVVM.View;
 
 namespace Expense_Tracker
 {
@@ -13,5 +12,32 @@ namespace Expense_Tracker
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ConfigureServices();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            // Register dependencies
+            serviceCollection.AddSingleton<DataAccess>();
+
+            serviceCollection.AddTransient<AddDataViewModel>();
+            serviceCollection.AddTransient<HomeViewModel>();
+            serviceCollection.AddTransient<AddDataView>();
+            serviceCollection.AddTransient<HomeView>();
+
+            serviceCollection.AddTransient<MainWindow>();
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
     }
 }
