cb94aa7c9f3dfdcaf5c44f1f5ee91e7d9c94f909
erik-lance <eriklance@gmail.com>
2023-08-27 12:27:26 +0800

    Added initial locator for view models for DI

---
+++ Core/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using Expense_Tracker.MVVM.View;
+using Expense_Tracker.MVVM.ViewModel;
+using System;
+
+namespace Expense_Tracker.Core
+{
+    internal class ViewModelLocator
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ViewModelLocator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public AddDataView GetAddDataView(AddDataViewModel viewModel)
+        {
+            return new AddDataView(viewModel);
+        }
+    }
+}
